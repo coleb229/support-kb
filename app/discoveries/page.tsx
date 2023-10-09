706ec26860cfe181305ad345e106ed669ea0ab19
Cole <coleb229@yahoo.com>
2023-10-09 15:19:40 -0700

    fixing deployment

--- app/discoveries/page.tsx
+++ app/discoveries/page.tsx
@@ -1,1 +1,13 @@
 "use server"
+import { getServerSession } from "next-auth"
+
+export default async function Discoveries() {
+  const session = await getServerSession()
+  const name = session?.user?.name
+  const firstName = name?.split(" ")[0]
+  return (
+    <div>
+      <h1>Hello {firstName}</h1>
+    </div>
+  )
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 'use server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
+import { newUser } from '@/lib/actions'
 
 export default async function Profile() {
   const session = await getServerSession(authOptions)
@@ -12,8 +13,15 @@ export default async function Profile() {
       <body>
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
           <h1>Profile</h1>
-          <p className='mb-20'>Email: {email}</p>
+          <p className=''>Email: {email}</p>
           <p className='mb-20'>Name: {name}</p>
+          <form action={newUser}>
+            <input type="text" name="email" id="email" value={email as string} hidden />
+            <input type="text" name="name" id="name" value={name as string} hidden />
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Add User
+            </button>
+          </form>
         </main>
       </body>
     </html>
