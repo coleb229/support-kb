2074abfee50e3596e5149e9bc47052ae0e0106d8
Cole <coleb229@yahoo.com>
2023-10-09 18:32:34 -0700

    added setup steps to the tiered loyalty guide

---
+++ app/articles/current/data/data.js
@@ -0,0 +1,22 @@
+export const mealSteps = [
+  'Enable the setting "Enable Meal Period Based Menu" in the portal or POS settings',
+  'Add a meal period profile in the portal under Settings --- Setup --- Meal Period --- Edit ---> + Add Meal Period',
+  'Set the name, days, and start/end times for the meal period and hit Save',
+  'Navigate to the catalog and locate the item/category that needs the meal period applied',
+  'Click on the item/category and it will open the item/category details page',
+  'Click on the "Meal Period" entry and select the meal period profile that was created',
+  'Click Save, then refresh the POS to see the changes reflected',
+]
+
+export const tieredLoyaltySteps = [
+  'Subscribe to the "Loyalty" module on the account from the portal under App Store ---> Loyalty',
+  'Open the loyalty module settings to set the initial loyalty settings',
+  'Set the name of the Reward Program',
+  'Set the rate at which points are redeemable for. (i.e. Conversion Rate)',
+  'Set the amount of points earned per dollar spent. (i.e. Accrual Rate)',
+  'Set the amount of points required before a reward can be redeemed. (i.e. Accrual Target)',
+  'Set the rate at which the points will expire. (i.e. Expire After)',
+  'Set the maximum number of points each customer can earn. (i.e. Max Limit)',
+  'Choose whether or not to allow for signup bonus points, and then set the bonus amount if true',
+  'Hit save and refresh the pos to see the changes reflected',
+]
