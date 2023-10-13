96d501653a24dabafb284f4e4a28c819d24d5e1f
Cole <coleb229@yahoo.com>
2023-10-13 01:26:02 -0700

    Can vercel handle simple image imports please

--- app/articles/current/TieredLoyaltyGuide/page.tsx
+++ app/articles/current/TieredLoyaltyGuide/page.tsx
@@ -1,13 +1,12 @@
 "use client"
-'test'
 import { data } from '../data/data'
 import { Walkthrough } from '@/components/articleComponents/Walkthrough'
 import { ArticleHeader } from '@/components/articleComponents/ArticleHeader'
 import { UseCases } from '@/components/articleComponents/UseCases'
 import ImageTabs from '@/components/articleComponents/ImageTabs'
-import s1 from '@/public/current/tieredLoyalty/s1.JPG'
-import s2 from '@/public/current/tieredLoyalty/s2.JPG'
-import s3 from '@/public/current/tieredLoyalty/s3.JPG'
+import s1 from '../../../../public/current/tieredLoyalty/s1.JPG'
+import s2 from '../../../../public/current/tieredLoyalty/s2.JPG'
+import s3 from '../../../../public/current/tieredLoyalty/s3.JPG'
 
 export default function MealPeriod() {
   const images = [s1, s2, s3]
