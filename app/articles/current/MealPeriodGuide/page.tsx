47f0fe25a223342fea251add7e6d750aee1b5add
Cole <coleb229@yahoo.com>
2023-10-13 01:15:36 -0700

    fixing deployment

--- app/articles/current/MealPeriodGuide/page.tsx
+++ app/articles/current/MealPeriodGuide/page.tsx
@@ -4,12 +4,12 @@ import { Walkthrough } from '@/components/articleComponents/Walkthrough'
 import { ArticleHeader } from '@/components/articleComponents/ArticleHeader'
 import { UseCases } from '@/components/articleComponents/UseCases'
 import ImageTabs from '@/components/articleComponents/ImageTabs'
-import s1 from '@/public/current/mealPeriod/1.jpg'
-import s2 from '@/public/current/mealPeriod/2.jpg'
-import s3 from '@/public/current/mealPeriod/3.jpg'
-import s4 from '@/public/current/mealPeriod/4.jpg'
-import s5 from '@/public/current/mealPeriod/5.jpg'
-import s6 from '@/public/current/mealPeriod/6.jpg'
+import s1 from '../../../../public/current/mealPeriod/1.jpg'
+import s2 from '../../../../public/current/mealPeriod/2.jpg'
+import s3 from '../../../../public/current/mealPeriod/3.jpg'
+import s4 from '../../../../public/current/mealPeriod/4.jpg'
+import s5 from '../../../../public/current/mealPeriod/5.jpg'
+import s6 from '../../../../public/current/mealPeriod/6.jpg'
 
 export default function MealPeriod() {
   const images = [s1, s2, s3, s4, s5, s6]
