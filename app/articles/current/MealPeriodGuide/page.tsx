6c4abf69aa98e87e164dbc87671b5a800b4a4678
Cole <coleb229@yahoo.com>
2023-10-13 02:04:43 -0700

    resorting to png

--- app/articles/current/MealPeriodGuide/page.tsx
+++ app/articles/current/MealPeriodGuide/page.tsx
@@ -4,12 +4,12 @@ import { Walkthrough } from '@/components/articleComponents/Walkthrough'
 import { ArticleHeader } from '@/components/articleComponents/ArticleHeader'
 import { UseCases } from '@/components/articleComponents/UseCases'
 import ImageTabs from '@/components/articleComponents/ImageTabs'
-import s1 from '@/public/current/mealPeriod/s1.jpg'
-import s2 from '@/public/current/mealPeriod/s2.jpg'
-import s3 from '@/public/current/mealPeriod/s3.jpg'
-import s4 from '@/public/current/mealPeriod/s4.jpg'
-import s5 from '@/public/current/mealPeriod/s5.jpg'
-import s6 from '@/public/current/mealPeriod/s6.jpg'
+import s1 from '@/public/current/mealPeriod/s1.png'
+import s2 from '@/public/current/mealPeriod/s2.png'
+import s3 from '@/public/current/mealPeriod/s3.png'
+import s4 from '@/public/current/mealPeriod/s4.png'
+import s5 from '@/public/current/mealPeriod/s5.png'
+import s6 from '@/public/current/mealPeriod/s6.png'
 
 export default function MealPeriod() {
   const images = [s1, s2, s3, s4, s5, s6]
--- app/articles/current/TieredLoyaltyGuide/page.tsx
+++ app/articles/current/TieredLoyaltyGuide/page.tsx
@@ -4,9 +4,9 @@ import { Walkthrough } from '@/components/articleComponents/Walkthrough'
 import { ArticleHeader } from '@/components/articleComponents/ArticleHeader'
 import { UseCases } from '@/components/articleComponents/UseCases'
 import ImageTabs from '@/components/articleComponents/ImageTabs'
-import s1 from '@/public/current/tieredLoyalty/s1.jpg'
-import s2 from '@/public/current/tieredLoyalty/s2.jpg'
-import s3 from '@/public/current/tieredLoyalty/s3.jpg'
+import s1 from '@/public/current/tieredLoyalty/s1.png'
+import s2 from '@/public/current/tieredLoyalty/s2.png'
+import s3 from '@/public/current/tieredLoyalty/s3.png'
 
 export default function MealPeriod() {
   const images = [s1, s2, s3]
