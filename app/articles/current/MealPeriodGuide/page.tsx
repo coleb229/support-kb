7b84d9ff4b2e57c2e2f3316c5e3fe9df73dab9bd
Cole <coleb229@yahoo.com>
2023-10-13 01:21:54 -0700

    trying this

--- app/articles/current/MealPeriodGuide/page.tsx
+++ app/articles/current/MealPeriodGuide/page.tsx
@@ -4,12 +4,12 @@ import { Walkthrough } from '@/components/articleComponents/Walkthrough'
 import { ArticleHeader } from '@/components/articleComponents/ArticleHeader'
 import { UseCases } from '@/components/articleComponents/UseCases'
 import ImageTabs from '@/components/articleComponents/ImageTabs'
-import s1 from '@/public/current/mealPeriod/s1.jpg'
-import s2 from '@/public/current/mealPeriod/s2.jpg'
-import s3 from '@/public/current/mealPeriod/s3.jpg'
-import s4 from '@/public/current/mealPeriod/s4.jpg'
-import s5 from '@/public/current/mealPeriod/s5.jpg'
-import s6 from '@/public/current/mealPeriod/s6.jpg'
+import s1 from '@/public/current/mealPeriod/s1.JPG'
+import s2 from '@/public/current/mealPeriod/s2.JPG'
+import s3 from '@/public/current/mealPeriod/s3.JPG'
+import s4 from '@/public/current/mealPeriod/s4.JPG'
+import s5 from '@/public/current/mealPeriod/s5.JPG'
+import s6 from '@/public/current/mealPeriod/s6.JPG'
 
 export default function MealPeriod() {
   const images = [s1, s2, s3, s4, s5, s6]
