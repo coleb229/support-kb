a7660248d80823018083c634261375b151a47a9a
Cole <coleb229@yahoo.com>
2023-10-14 00:11:33 -0700

    Much needed expansion functionality added to the image slider component

--- components/articleComponents/ImageTabs.tsx
+++ components/articleComponents/ImageTabs.tsx
@@ -3,6 +3,19 @@ import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import Slider from "react-slick";
 import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+
 
 export default function ImageTabs({images}:any) {
   const settings = {
@@ -17,18 +30,39 @@ export default function ImageTabs({images}:any) {
 
   return (
     <div className="w-full rounded-lg bg-slate-300 my-4">
-      <div className="w-2/3 mx-auto my-10">
+      <div className="w-1/2 mx-auto my-10">
         <Slider {...settings}>
           {images.map((image:any, index:number) => (
             count++,
             <div key={index}>
               <p className="text-center text-2xl font-bold">Step {count}</p>
-              <Image
-                src={image}
-                alt="Meal Period"
-                width={700}
-                className="mx-auto"
-              />
+              <AlertDialog>
+                <AlertDialogTrigger className="cursor-pointer">
+                  <Image
+                    src={image}
+                    alt="Steps"
+                    width={700}
+                    className="mx-auto"
+                  />
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Step {count}</AlertDialogTitle>
+                  </AlertDialogHeader>
+                  <AlertDialogDescription>
+                    <Image
+                      src={image}
+                      alt="Steps"
+                      className="mx-auto w-screen"
+                    />
+                  </AlertDialogDescription>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>
+                      <Button variant='destructive'>Gotcha!</Button>
+                    </AlertDialogCancel>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
           ))}
         </Slider>
